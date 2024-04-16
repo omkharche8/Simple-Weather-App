e84c83dca63c361581e012dad90de7b00ac1d1ea
Om Kharche <orkharche@gmail.com>
2024-04-16 16:28:14 +0530

    first commit

---
+++ weatherapp/lib/pages/weather_page.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weatherapp/models/weather_model.dart';
+import 'package:weatherapp/services/weather_services.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService('33f931470e8f7d2cb9882ce90d918991');
+  Weather? _weather;
+
+  //fetch
+  _fetchWeather() async {
+    //get current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather ccity
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+
+    //any errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  //weather animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json';
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  //init state
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch weather on start
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 31, 35, 44),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //city name
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  _weather?.cityName ?? "loading city...",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.white),
+                ),
+              ),
+
+              //animations
+              Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+              //temperature
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('${_weather?.temperature.round()}°C',
+                    style: TextStyle(fontSize: 20, color: Colors.white)),
+              ),
+
+              //weather conditions
+              Padding(
+                  padding: EdgeInsets.all(2),
+                  child: Text(
+                    _weather?.mainCondition ?? "",
+                    style: TextStyle(color: Colors.white),
+                  ))
+            ],
+          ),
+        ));
+  }
+}
---
+++ weatherapp/lib/services/weather_services.dart
@@ -0,0 +1,44 @@
+import 'dart:convert';
+
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:weatherapp/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load Data');
+    }
+  }
+
+  Future<String> getCurrentCity() async {
+    //get user permission
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    //fetch location
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+
+    //convert location into a list of placemark objects
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    //extract the city name
+    String? city = placemarks[0].locality;
+
+    return city ?? "";
+  }
+}
